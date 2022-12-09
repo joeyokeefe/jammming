f6a68280dcb679117a9ebaffd8a9c7e0548851a3
joseph okeefe <joeyokeefe5@gmail.com>
2022-12-08 19:47:27 -0500

    Set up component folders

---
+++ src/Components/SearchResults/SearchResults.js
@@ -0,0 +1,13 @@
+import "./SearchResults.css";
+import { TrackList } from "../TrackList/TrackList";
+
+class SearchResults extends React.Component {
+  render() {
+    <div className="SearchResults">
+      <h2>Results</h2>
+      {/* <!-- Add a TrackList component --> */}
+    </div>;
+  }
+}
+
+export default SearchResults;
